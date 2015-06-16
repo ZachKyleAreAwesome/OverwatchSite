8de2abcc24d1dc0ae9353abedcc63a349dd65b2e
kdraving <kdraving@gmail.com>
2015-06-16 09:22:14 -0400

    Added basic html/php files to edit

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+<?php include("header.php"); ?>
+    <div class="mainContent">
+      <h1>Header in here</h1>
+      <p>Paragraph in here</p>
+    </div>
+<?php include("footer.php"); ?>
